5620200faecf69787c510a4e2d16cc4797e6c045
deibytb <deiby@devacademy.la>
2016-09-27 17:06:33 -0500

    Add ship asset

--- devGame/GameScene.swift
+++ devGame/GameScene.swift
@@ -9,14 +9,28 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var ship = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        let shipTexture = SKTexture(imageNamed: "Ship.png")
+        let shipTexture2 = SKTexture(imageNamed: "Ship2.png")
+        
+        let animation = SKAction.animateWithTextures([shipTexture, shipTexture2], timePerFrame: 0.2)
+        let makeShipFlap = SKAction.repeatActionForever(animation)
+        
+        ship = SKSpriteNode(texture: shipTexture)
+        
+        ship.xScale = 0.5
+        ship.yScale = 0.5
+        
+        ship.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        
+        ship.runAction(makeShipFlap)
+        
+        self.addChild(ship)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
